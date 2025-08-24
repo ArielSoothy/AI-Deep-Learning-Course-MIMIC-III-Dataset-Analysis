cca773950619249bcf86bb279aa6d896582c45bc
Ariel <arielsoothy@gmail.com>
2025-08-24 22:24:43 +0300

    Fix vite base path for correct GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/AI-Deep-Learning-Essentials/',
+  base: '/AI-Deep-Learning-Course-MIMIC-III-Dataset-Analysis/',
 })
